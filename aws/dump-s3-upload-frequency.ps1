b8265f280d6ba7f7e00bc2ddc787c8ef3c77d788
ach8o <andrew.chumachenko@observe.ai>
2021-10-01 10:53:02 -0700

    Updated description

--- aws/dump-s3-upload-frequency.ps1
+++ aws/dump-s3-upload-frequency.ps1
@@ -11,6 +11,14 @@
 
         LastModieid - String representing the date stamp to dump uploads after
             Example: "2021-09-27"
+
+.SYNOPSIS
+    # start powershell
+    > pwsh
+    # run the script
+    > ./dump-s3-upload-frequency.ps1 -Bucket '<bucket-name>' -Prefix '<s3-prefix>' -LastModified 'yyyy-MM-dd'
+    # The script scans S3 & produces CSV file in the same folder it is executed in
+
 #>  
 
 param (
