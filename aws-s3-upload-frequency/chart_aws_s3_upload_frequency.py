9cf5ec9b937846af9345e63366d3cdabefcf2cb4
ach8o <andrew.chumachenko@observe.ai>
2021-10-21 15:01:41 -0700

    Implemented listing LastModifiedTime on S3 objects

--- aws-s3-upload-frequency/chart_aws_s3_upload_frequency.py
+++ aws-s3-upload-frequency/chart_aws_s3_upload_frequency.py
@@ -9,26 +9,32 @@ Approach:
     * Save the chart as image in the local folder
 
 Parameters:
-    --s3-path s3://<bucket>/s3/path/
+    --s3-url s3://<bucket>/s3/path/
         S3 path to process the files under ( recursively )
     --output-label <customer-name>
         Used to generate output files
         <customer-name>-list-objects-out.json
         <customer-name>-uploads-frequqncy.png
 """
+import boto3
 import argparse
+from tqdm import tqdm
+from urllib.parse import urlparse
+
+class CONTEXT:
+    s3_data = []
 
 class CONFIG:
     class ARGS:
-        s3path = None,
-        label  = None 
+        s3url = None,
+        label = None 
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description=__doc__, 
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument(
-        '--s3-path',
+        '--s3-url',
         required=True,
         help='S3 path to scan recursively')
     parser.add_argument(
@@ -36,10 +42,28 @@ def parse_args():
         required=True, 
         help='Label to use for output files (dashboard name, for example)')
     args = parser.parse_args()
-    CONFIG.ARGS.s3path = args.s3_path
+    CONFIG.ARGS.s3url = args.s3_url
     CONFIG.ARGS.label  = args.output_label
 
 
+def scan_s3_path():
+    client = boto3.client('s3')
+    s3_url = urlparse(CONFIG.ARGS.s3url)
+    s3_bucket = s3_url.netloc
+    s3_prefix = s3_url.path[1:]
+    paginator = client.get_paginator('list_objects_v2')
+    listing = {}
+    pbar_iterator = tqdm(paginator.paginate(Bucket=s3_bucket, Prefix=s3_prefix))
+    pbar_iterator.set_description_str('Iterating over S3 API response pages')
+    for page in pbar_iterator:
+        try:
+            page_contents = page['Contents']
+            listing.update( { rec['Key'] : rec['LastModified'] for rec in page_contents } )
+        except KeyError:
+            # Ignoring KeyError that may occur on "folders" (empty objects)
+            pass
+    return listing
+
 if __name__ == '__main__':
     parse_args()
-    print(CONFIG.ARGS.s3path, CONFIG.ARGS.label, sep=', ')
+    print(CONFIG.ARGS.s3url, CONFIG.ARGS.label, sep=', ')
